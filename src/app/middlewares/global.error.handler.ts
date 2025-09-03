f690512a8f98d7bb010ad24574075ea817e26df5
sohun-cs <asaduzzamansohun5@gmail.com>
2025-09-03 18:18:05 +0600

    Added: Different Error Handlers

--- src/app/middlewares/global.error.handler.ts
+++ src/app/middlewares/global.error.handler.ts
@@ -10,10 +10,52 @@ export const globalErrorHandler = async (err: any, req: Request, res: Response, 
     let statusCode = 500;
     let message = `Something wend wrong.`;
 
-    if (err instanceof AppError) {
+    const errorSources: any = [];
+
+    if (err.code === 11000) {
+
+        const matchedArray = err.message.match(/"([^"]*)"/)
+        statusCode = 400;
+        message = `${matchedArray[1]} already exists!!`
+    }
+
+    else if (err.name === 'CastError') {
+        statusCode = 400;
+        message = "Cast Error"
+    }
+
+    else if (err.name === "ZodError") {
+
+        statusCode = 400;
+        message = "Zod Error";
+
+        err.issues.forEach((issue: any) => {
+            errorSources.push({
+                path: issue.path[issue.path.length - 1],
+                message: issue.message
+            })
+        })
+
+    }
+
+    else if (err.name === 'ValidationError') {
+        statusCode = 400;
+        const errors = Object.values(err.errors);
+
+        errors.forEach((errorObject: any) => errorSources.push({
+            path: errorObject.path,
+            message: errorObject.message
+        }));
+
+        message = err.message
+    }
+
+    else if (err instanceof AppError) {
         statusCode = err.statusCode
         message = err.message
-    } else if (err instanceof Error) {
+    }
+
+    else if (err instanceof Error) {
         statusCode = 500;
         message = err.message
     }
@@ -22,7 +64,7 @@ export const globalErrorHandler = async (err: any, req: Request, res: Response, 
     res.status(statusCode).json({
         success: false,
         message: message,
-        err,
+        errorSources,
         stack: envVars.NODE_ENV === 'development' ? err.stack : null
     })
 
